8c11593d2eb1d6475b003b46318e22b4efbf79d8
matthewcodesthings <matthewcodesthings@gmail.com>
2017-03-07 22:16:19 -0800

    Validating textboxes

--- javascript.js
+++ javascript.js
@@ -1,11 +1,19 @@
-var text = document.getElementById("text");
+/**
+ * [Valid inputs for the text field]
+ * @type {Array}
+ */
+var validNames = ["Matthew", "Matt"];
 
-text.innerHTML = "text";
-
-text.onmouseover = function() {
-    text.style.size = 32;
-};
-
-text.onmouseout = function() {
-    text.style.size = 16;
-};
+/**
+ * [Iterates through the validNames array and checks if the
+ * text field has a valid input]
+ * @return {[alert]} [Tells user they are logged in]
+ */
+function checkName() {
+	var textField = document.getElementById("fieldName");
+	for (var i = 0; i < validNames.length; i++) {
+		if (textField.value == validNames[i]) {
+			return alert("You are " + validNames[i]);
+		}
+	}
+}
