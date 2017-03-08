64dc17c95309643d89dae364543179383f911f9d
matthewcodesthings <matthewcodesthings@gmail.com>
2017-03-07 22:39:03 -0800

    Trying out TypeScript

--- javascript.ts
+++ javascript.ts
@@ -3,16 +3,17 @@
  * @type {Array}
  */
 var validNames = ["Matthew", "Matt"];
+
 /**
  * [Iterates through the validNames array and checks if the
  * text field has a valid input]
  * @return {[alert]} [Tells user they are logged in]
  */
 function checkName() {
-    var textField = document.getElementById("fieldName");
-    for (var i = 0; i < validNames.length; i++) {
-        if (textField.value == validNames[i]) {
-            return alert("You are " + validNames[i]);
-        }
-    }
+	var textField = (<HTMLInputElement>document.getElementById("fieldName"));
+	for (var i = 0; i < validNames.length; i++) {
+		if (textField.value == validNames[i]) {
+			return alert("You are " + validNames[i]);
+		}
+	}
 }
